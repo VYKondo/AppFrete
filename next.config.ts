78758d089c004fdd0f620e9adfdbf375e1591da7
Yudi <viniciusyudikondo@gmail.com>
2026-02-16 15:10:07 -0300

    Agora sim, versão atualizada

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  // Isso desativa a tentativa de pré-renderizar páginas estáticas que dão erro
-  output: 'standalone', 
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactCompiler: true,
 };
 
 export default nextConfig;
